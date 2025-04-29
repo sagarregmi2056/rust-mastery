308094b7ad9deef0313c541c032358ff82902f50
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2025-04-29 23:09:04 +0545

    Initial comment

---
+++ days/day1/ownership.rs
@@ -0,0 +1,9 @@
+// days/day1/ownership.rs
+pub fn demo() {
+    let s = String::from("ownership");
+    println!("{}", s);
+}
+
+pub fn main() {  // Entry point
+    demo();      // Call your function
+}
